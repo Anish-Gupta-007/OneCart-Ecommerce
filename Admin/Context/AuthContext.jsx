8d8cac17aa523f09e32b81612cf0bbe771195427
Anish Gupta <anishprogrammer007@gmail.com>
2025-11-11 19:33:04 +0530

    Update server URL in AuthContext

--- Admin/Context/AuthContext.jsx
+++ Admin/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from "react";
 export const authDataConetxt = createContext();
 const AuthContext = ({ children }) => {
-  const serverUrl = "https://onecart-ecommerceadminn.onrender.com";
+  const serverUrl = "https://onecart-ecommerce-backend.onrender.com";
   const value = {
     serverUrl,
   };
